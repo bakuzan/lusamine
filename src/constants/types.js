d86b2680efab6bac2c982e01a9ff5c7e539b4e65
bakuzan <imannoyed4days@hotmail.co.uk>
2018-05-06 21:03:50 +0100

    starting adding pokemon. added type enums.

---
+++ src/constants/types.js
@@ -0,0 +1,20 @@
+export default Object.freeze({
+    normal: 1,
+    grass: 2,
+    fire: 3,
+    water: 4,
+    fighting: 5,
+    flying: 6,
+    poison: 7,
+    ground: 8,
+    rock: 9,
+    bug: 10,
+    ghost: 11,
+    electric: 12,
+    psychic: 13,
+    ice: 14,
+    dragon: 15,
+    dark: 16,
+    steel: 17,
+    fairy: 18
+});
