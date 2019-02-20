9d2e48a63b23e903503174a9adb5a80727983b52
Steven Walsh <steven.walsh@worksmart.co.uk>
2019-02-20 19:00:46 +0000

    Updates

---
+++ precache-manifest.c650d2565c5fee89b894c077fc2ad2c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0c60420cabc448a5d9d22f82512e021",
+    "url": "/lusamine/static/media/art.f0c60420.webp"
+  },
+  {
+    "revision": "27843b48fcb61f3986cc",
+    "url": "/lusamine/static/css/main.9b21abf4.chunk.css"
+  },
+  {
+    "revision": "b0c5061a7b3c8cfd189a",
+    "url": "/lusamine/static/js/runtime~main.b0c5061a.js"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "/lusamine/static/media/roboto-mono.e92cc0fb.woff2"
+  },
+  {
+    "revision": "1dd7fe46e98e840183f2",
+    "url": "/lusamine/static/js/2.1dd7fe46.chunk.js"
+  },
+  {
+    "revision": "bfb4b1f22137c60afd8aa5c009d920bb",
+    "url": "/lusamine/static/media/types.bfb4b1f2.png"
+  },
+  {
+    "revision": "27843b48fcb61f3986cc",
+    "url": "/lusamine/static/js/main.27843b48.chunk.js"
+  },
+  {
+    "revision": "520c5cad9346e603ac7c18a6a5ba917c",
+    "url": "/lusamine/static/media/art.520c5cad.png"
+  },
+  {
+    "revision": "cf9fc0079e261f9c48bff9f3c81f1410",
+    "url": "/lusamine/static/media/sprites.cf9fc007.png"
+  },
+  {
+    "revision": "96ef307ee32a542b7951688080cb2d69",
+    "url": "/lusamine/static/media/sprites.96ef307e.webp"
+  },
+  {
+    "revision": "1dd7fe46e98e840183f2",
+    "url": "/lusamine/static/css/2.0c208623.chunk.css"
+  },
+  {
+    "revision": "5fae266a2fa78b02b42931b5de561778",
+    "url": "/lusamine/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lusamine/precache-manifest.cb570793ecf404099dab3c72bbf43e96.js"
+  "/lusamine/precache-manifest.c650d2565c5fee89b894c077fc2ad2c0.js"
 );
 
 workbox.clientsClaim();
