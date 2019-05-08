27fec8c7fccf400acd2d27a43064e92731a3df43
bakuzan <imannoyed4days@hotmail.co.uk>
2019-05-08 13:17:32 +0100

    Updates

---
+++ precache-manifest.51ec965a8ce7ee38657d5ba1f0454feb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e719b16d11a469761b00a05f82e8b51",
+    "url": "/lusamine/index.html"
+  },
+  {
+    "revision": "2c6e455272826be6030b",
+    "url": "/lusamine/static/css/main.32e11c7a.chunk.css"
+  },
+  {
+    "revision": "c25a83845056d7e35fd9",
+    "url": "/lusamine/static/js/2.970d8021.chunk.js"
+  },
+  {
+    "revision": "2c6e455272826be6030b",
+    "url": "/lusamine/static/js/main.f8e62b9e.chunk.js"
+  },
+  {
+    "revision": "fa521e0fd32484deed69",
+    "url": "/lusamine/static/js/runtime~main.a2c07b01.js"
+  },
+  {
+    "revision": "520c5cad9346e603ac7c18a6a5ba917c",
+    "url": "/lusamine/static/media/art.520c5cad.png"
+  },
+  {
+    "revision": "f0c60420cabc448a5d9d22f82512e021",
+    "url": "/lusamine/static/media/art.f0c60420.webp"
+  },
+  {
+    "revision": "0d0730eb672e016c90dcf1d71c8ea42a",
+    "url": "/lusamine/static/media/pokemon-opening.0d0730eb.ogg"
+  },
+  {
+    "revision": "7140bc359bb73b6a5f144edeff877808",
+    "url": "/lusamine/static/media/pokemon-opening.7140bc35.mp3"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "/lusamine/static/media/roboto-mono.e92cc0fb.woff2"
+  },
+  {
+    "revision": "96ef307ee32a542b7951688080cb2d69",
+    "url": "/lusamine/static/media/sprites.96ef307e.webp"
+  },
+  {
+    "revision": "cf9fc0079e261f9c48bff9f3c81f1410",
+    "url": "/lusamine/static/media/sprites.cf9fc007.png"
+  },
+  {
+    "revision": "bfb4b1f22137c60afd8aa5c009d920bb",
+    "url": "/lusamine/static/media/types.bfb4b1f2.png"
+  },
+  {
+    "revision": "ecadccdca3265038f4b8495611daa012",
+    "url": "/lusamine/static/media/types.ecadccdc.webp"
+  }
+]);
