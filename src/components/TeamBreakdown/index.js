669699621267489dca37a15afbf5afd7f74d4c8e
bakuzan <imannoyed4days@hotmail.co.uk>
2019-06-10 11:59:31 +0100

    prevented render over css hiding.

--- src/components/TeamBreakdown/index.js
+++ src/components/TeamBreakdown/index.js
@@ -50,6 +50,9 @@ class TeamBreakdown extends React.Component {
     const isCollapsed = !alwaysOpen && this.state.isCollapsed;
     const renderNonTypes = !typeBreakdownOnly;
     const noMembers = members.size === 0;
+    const showToggle = !alwaysOpen && !noMembers;
+    const showContent = (!isCollapsed && !noMembers) || alwaysOpen;
+
     const mouseState = {
       onMouseEnter: this.handleMouseEnterLeave,
       onMouseLeave: this.handleMouseEnterLeave
@@ -64,88 +67,82 @@ class TeamBreakdown extends React.Component {
 
     return (
       <div className={classNames('team-breakdown', className)}>
-        {!alwaysOpen && (
+        {showToggle && (
           <Button
             id="toggle-breakdown"
             isAction
-            className={classNames('team-breakdown__action', {
-              'team-breakdown__action--hide': noMembers
-            })}
+            className={classNames('team-breakdown__action')}
             onClick={this.handleToggle}
           >
             {isCollapsed ? `Show team breakdown` : `Hide team breakdown`}
           </Button>
         )}
-        <div
-          className={classNames(
-            'team-breakdown__content',
-            {
-              'team-breakdown__content--collapsed': isCollapsed
-            },
-            contentClassName
-          )}
-        >
-          {renderNonTypes && (
+        {showContent && (
+          <div
+            className={classNames('team-breakdown__content', contentClassName)}
+          >
+            {renderNonTypes && (
+              <List
+                className={classNames(
+                  'team-breakdown__list',
+                  'stat-breakdown-list'
+                )}
+                items={statCounts}
+                itemTemplate={(item) => (
+                  <li
+                    key={item.key}
+                    className={classNames(
+                      'team-breakdown__item',
+                      'stat-breakdown-list__item'
+                    )}
+                  >
+                    <div className={classNames('breakdown-item-title')}>
+                      <div className={classNames('breakdown-item-title__text')}>
+                        {item.key}
+                      </div>
+                    </div>
+                    <StatBreakdownPanel
+                      id={item.key}
+                      nameSource={item.getKeyName}
+                      data={item.counts}
+                      onMouseState={{ ...mouseState }}
+                    />
+                  </li>
+                )}
+              />
+            )}
             <List
+              columns={1}
               className={classNames(
                 'team-breakdown__list',
-                'stat-breakdown-list'
+                'type-breakdown-list'
               )}
-              items={statCounts}
+              items={weaknessCounts}
               itemTemplate={(item) => (
                 <li
                   key={item.key}
                   className={classNames(
                     'team-breakdown__item',
-                    'stat-breakdown-list__item'
+                    'type-breakdown-list__item'
                   )}
                 >
                   <div className={classNames('breakdown-item-title')}>
                     <div className={classNames('breakdown-item-title__text')}>
-                      {item.key}
+                      {capitaliseEachWord(item.key)}
                     </div>
                   </div>
-                  <StatBreakdownPanel
+                  <EffectiveTypePanel
                     id={item.key}
-                    nameSource={item.getKeyName}
+                    panelModifier={item.goodCountModifier}
+                    types={types}
                     data={item.counts}
                     onMouseState={{ ...mouseState }}
                   />
                 </li>
               )}
             />
-          )}
-          <List
-            columns={1}
-            className={classNames(
-              'team-breakdown__list',
-              'type-breakdown-list'
-            )}
-            items={weaknessCounts}
-            itemTemplate={(item) => (
-              <li
-                key={item.key}
-                className={classNames(
-                  'team-breakdown__item',
-                  'type-breakdown-list__item'
-                )}
-              >
-                <div className={classNames('breakdown-item-title')}>
-                  <div className={classNames('breakdown-item-title__text')}>
-                    {capitaliseEachWord(item.key)}
-                  </div>
-                </div>
-                <EffectiveTypePanel
-                  id={item.key}
-                  panelModifier={item.goodCountModifier}
-                  types={types}
-                  data={item.counts}
-                  onMouseState={{ ...mouseState }}
-                />
-              </li>
-            )}
-          />
-        </div>
+          </div>
+        )}
       </div>
     );
   }
