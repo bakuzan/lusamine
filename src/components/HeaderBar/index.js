3f82008030dff4ba3c16f16d0f75321789b6baff
bakuzan <imannoyed4days@hotmail.co.uk>
2019-11-15 08:28:16 +0000

    Use new scroll hook

--- src/components/HeaderBar/index.js
+++ src/components/HeaderBar/index.js
@@ -2,55 +2,34 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import createListeners from 'ayaka/createListeners';
 import Header from 'meiko/Header';
 import Image from 'meiko/Image';
+import { useScrollPosition } from 'meiko/hooks/useScrollPosition';
 import NavigationMenu from './NavigationMenu';
-import { getWindowScrollPosition } from 'utils/common';
+
 import Logo from 'assets/logo.png';
 
 import './HeaderBar.scss';
 
-class HeaderBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      windowScrollPosition: 0
-    };
-  }
+const HeaderBar = React.memo(({ pageTitle }) => {
+  const windowScrollPosition = useScrollPosition();
 
-  componentDidMount() {
-    this.scrollListeners = createListeners('scroll', () => {
-      const windowScrollPosition = getWindowScrollPosition();
-      if (windowScrollPosition !== this.state.windowScrollPosition) {
-        this.setState({ windowScrollPosition });
-      }
-    })();
-    this.scrollListeners.listen();
-  }
+  const isPageScrolled = !!windowScrollPosition;
+  const headerClasses = classNames('header-bar', {
+    'header-bar--page-scrolled': isPageScrolled
+  });
 
-  componentWillUnmount() {
-    this.scrollListeners.remove();
-  }
-
-  render() {
-    const isPageScrolled = !!this.state.windowScrollPosition;
-    const headerClasses = classNames('header-bar', {
-      'header-bar--page-scrolled': isPageScrolled
-    });
-
-    return (
-      <Header
-        id="lusamine-header"
-        className={headerClasses}
-        leftAlignTitle
-        title={this.props.pageTitle}
-        navLeft={<Image className="logo" alt="Pokémon Logo" src={Logo} />}
-        navRight={<NavigationMenu />}
-      />
-    );
-  }
-}
+  return (
+    <Header
+      id="lusamine-header"
+      className={headerClasses}
+      leftAlignTitle
+      title={pageTitle}
+      navLeft={<Image className="logo" alt="Pokémon Logo" src={Logo} />}
+      navRight={<NavigationMenu />}
+    />
+  );
+});
 
 HeaderBar.propTypes = {
   pageTitle: PropTypes.string.isRequired
