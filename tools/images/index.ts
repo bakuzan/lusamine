e3f4bb73515b8538f213d7578ce63fa84b8bcb30
bakuzan <imannoyed4days@hotmail.co.uk>
2020-02-20 09:54:05 +0000

    Added missing art. Updated art sheets.

--- tools/images/index.ts
+++ tools/images/index.ts
@@ -3,24 +3,26 @@ import fs from 'fs';
 import path from 'path';
 import { ask } from 'stdio';
 import { promisify } from 'util';
+import stream from 'stream';
 
 import { capitalise } from 'ayaka/capitalise';
 import { createClient } from 'medea';
 import { constructPokedex } from '@src/data';
 
 import fetchPage from '@/readCachedFile';
-import Enums from '@/enums';
+import Enums, { ImageScrapeTarget } from '@/enums';
 import Mappers from '@/mappers';
 import { checkImgForVariant } from '@/processors';
-import { prop } from '@/utils';
+import { getFirstValidAttributeValue, prop } from '@/utils';
+import got from 'got/dist/source';
 
 const writeAsync = promisify(fs.writeFile);
+const pipeline = promisify(stream.pipeline);
 
 const POKEMON = 'pokemon';
 const POKEMON_URL =
   'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number';
 
-const scrapeTargets = ['sprites', 'art'];
 const NO_IMAGE = 'Image does not exist';
 const NO_ENTRY = "Current items doesn't contain the entry";
 
@@ -62,7 +64,8 @@ async function run() {
       option: 'key',
       shortcut: 'k',
       description: `Image scrape target, either "sprites" or "art"`,
-      validate: (_, value: string) => scrapeTargets.includes(value)
+      validate: (_, value: string) =>
+        Object.values<string>(ImageScrapeTarget).includes(value)
     })
     .parse(process.argv)
     .welcome();
@@ -182,28 +185,51 @@ async function run() {
   const filename = path.join(__dirname, `./missing_${imageFolder}.json`);
   await writeAsync(filename, JSON.stringify(missing, null, 2));
 
-  // TODO
-  // Refactor stdio code in medea
-  const answer = await ask('Would you like to download missing images?', {
-    options: [...yeses, ...noes],
-    maxRetries: 2
-  });
-
-  if (yeses.includes(answer)) {
+  if (missing.length) {
     // TODO
-    // Download missing
-  } else {
-    console.log(
-      chalk.yellowBright(
-        'The following counts do not include mega evolutions (48 as of Gen 8) or different base pokemon forms.'
-      )
-    );
-    console.log(
-      chalk.blue(
-        `Of the ${items.length} Items processed, ${missing.length} were deemed missing.`
-      )
-    );
+    // Refactor stdio code in medea
+    const answer = await ask('Would you like to download missing images?', {
+      options: [...yeses, ...noes],
+      maxRetries: 2
+    });
+
+    if (yeses.includes(answer)) {
+      if (imageFolder === ImageScrapeTarget.Art) {
+        for (const item of missing) {
+          const imageFilename = path.join(
+            __dirname,
+            `./${imageFolder}/${item.filename}`
+          );
+
+          const $page = await fetchPage(item.filename, item.art);
+          const href = getFirstValidAttributeValue($page, [
+            {
+              selector: '.mw-filepage-other-resolutions > a:first-child',
+              attr: 'href'
+            },
+            { selector: '.fullMedia > a:first-child', attr: 'href' }
+          ]);
+
+          const url = `https:${href}`;
+          await pipeline(got.stream(url), fs.createWriteStream(imageFilename));
+        }
+      } else if (imageFolder === ImageScrapeTarget.Sprites) {
+        // TODO
+        // Download missing sprites...
+      }
+    }
   }
+
+  console.log(
+    chalk.yellowBright(
+      'The following counts do not include mega evolutions (48 as of Gen 8) or different base pokemon forms.'
+    )
+  );
+  console.log(
+    chalk.blue(
+      `Of the ${items.length} Items processed, ${missing.length} were deemed missing.`
+    )
+  );
 
   console.log(chalk.blue('Finished successfully!'));
 
--- tools/utils.ts
+++ tools/utils.ts
@@ -1,3 +1,19 @@
+import { ScrapeOption } from './types/ScrapeOption';
+
 export function prop<T, K extends keyof T>(obj: T, key: K) {
   return obj[key];
+}
+
+export function getFirstValidAttributeValue(
+  $: CheerioStatic,
+  items: ScrapeOption[]
+) {
+  for (const item of items) {
+    const value = $(item.selector).attr(item.attr);
+    if (value) {
+      return value;
+    }
+  }
+
+  throw new Error('Not valid value found');
 }
